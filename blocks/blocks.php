07f57e521dc0343bad4bcd9ffc550a3e585ad343
Tsjippy <ewaldha@hotmail.com>
2026-07-06 11:25:55 +0200

    skip:

--- blocks/blocks.php
+++ blocks/blocks.php
@@ -11,13 +11,13 @@ function blockInit()
 {
     register_block_type(
         __DIR__ . '/embedPage/build',
-        array(
+        [
             'render_callback' => function( $attributes){
                 $page    = json_decode($attributes['page']);
                 if (isset($page->ID)) {
                     return displayPageContents($page->ID, $attributes['hide'], $attributes['newline']);
                 }
-            },
-        )
+            }
+        ]
     );
 }
