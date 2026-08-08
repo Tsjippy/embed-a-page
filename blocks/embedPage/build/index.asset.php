20c35bb3802cf68d8ecdbe718e204fac344f1581
Tsjippy <ewaldha@hotmail.com>
2026-08-08 16:56:00 +0200

    blocks js

--- blocks/embedPage/build/index.asset.php
+++ blocks/embedPage/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => 'c9cbc6d0a2ec9f1eb882');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '270e240b87c797b52a6d');
